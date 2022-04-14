c977db66b1bd6e3eaafb6a1f324c849d3470b826
Roger Santos <rogerioantoniodossantos@outlook.com>
2022-04-13 21:55:26 -0300

    feat: Classe Decimal adicionada

--- src/Round.php
+++ src/Round.php
@@ -2,27 +2,14 @@
 
 namespace SanotsRoger\ENumbers;
 
+use Decimal\Decimal;
+
 class Round
 {
-    public static function ruleABNT(float $num): float
+    public static function ruleABNT(float $num)
     {
-        $num_arr = explode('.', $num);
+        $n = new Decimal((string)$num);
 
-        if (!isset($num_arr[1]) || (strlen($num_arr[1]) < 3)) {
-            return $num;
-        }
-
-        $units = ltrim($num_arr[0], 0);
-        $cents = substr($num_arr[1], 0, 2);
-        $extra = rtrim(substr($num_arr[1], 2), 0);
-        $mils = substr($extra, 0, 1);
-
-        if (($mils < 5) || (($extra == 5) && !($cents % 2))) {
-            $number_round = $units . '.' . $cents;
-        } else {
-            $number_round = round($num, 2, PHP_ROUND_HALF_UP);
-        }
-
-        return number_format($number_round, 2, '.', '');
+        return floatval($n->round(2));
     }
 }
